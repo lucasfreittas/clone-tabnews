05e37bd247048114ab1bea6d1b4c2c18534d7f0b
Lucas Freitas <l.freittas@outlook.com>
2026-08-25 21:43:43 -0700

    Setup prettier  and

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return(
-        <h1>I love you neni, and I'll always love you ❤️❤️</h1>  
-    )
+  return <h1>I love you neni, and I'll always love you ❤️❤️</h1>;
 }
 
 export default Home;
